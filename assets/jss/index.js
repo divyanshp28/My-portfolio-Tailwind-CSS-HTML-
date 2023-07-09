f9205f3965674526b8b930eb13b2c5bb1c5d4bc1
Divyansh <divyanshji1707@gmail.com>
2023-07-09 19:43:21 +0530

    FInal project

---
+++ assets/jss/index.js
@@ -0,0 +1,6 @@
+var typed = new Typed('.auto-input',{
+    strings: ['Data Scientist', 'Data Analyst', 'Gamer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true
+})
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'space': "url('/assets/img/bg-2.jpg')",
+      },
+    },
+  },
+  plugins: [],
+}
+
